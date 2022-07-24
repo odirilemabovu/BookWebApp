9d30d9e1881f181eba88ad7c1188b401c36a5f6b
OdirileMabovu <106153646+odirilemabovu@users.noreply.github.com>
2022-07-24 23:58:40 +0200

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}

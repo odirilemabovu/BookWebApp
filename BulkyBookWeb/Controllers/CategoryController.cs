2efb6ceded4afefaf90c5f2aaf3d1cded3034871
OdirileMabovu <106153646+odirilemabovu@users.noreply.github.com>
2022-07-25 15:36:58 +0200

    Implement N-Tier Architecture

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -39,6 +39,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category Created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -74,6 +75,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully ";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -98,7 +100,7 @@ namespace BulkyBookWeb.Controllers
         }
 
         //POST
-        [HttpPost]
+        [HttpPost,ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public IActionResult DeletePOST(int? id)
         {
@@ -110,7 +112,8 @@ namespace BulkyBookWeb.Controllers
          
                 _db.Categories.Remove(obj);
                 _db.SaveChanges();
-                return RedirectToAction("Index");
+            TempData["success"] = "Category deleted successfully ";
+            return RedirectToAction("Index");
         }
     }
 }
